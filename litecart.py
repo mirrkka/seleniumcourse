2af65e25576518a74fbf54e3a35694af449ce682
mirrkka <aleksandraryzhkova14@gmail.com>
2018-09-20 16:41:58 +0300

    some change
    change

--- litecart.py
+++ litecart.py
@@ -5,11 +5,11 @@ from driver_fixture import *
 
 def login(driver):
     driver.get("http://localhost/litecart/admin/")
-
-
-def test_login(driver):
-    login(driver)
     driver.find_element_by_xpath("//input[@name='username']").send_keys("admin")
     driver.find_element_by_xpath("//input[@name='password']").send_keys("admin")
     driver.find_element_by_xpath("//button[@name='login']").click()
     driver.quit()
+
+
+def test_login(driver):
+    login(driver)
