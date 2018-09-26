9373aa5bccd4a2f09444e22e580cc99c98e169b2
mirrkka <aleksandraryzhkova14@gmail.com>
2018-09-26 17:43:43 +0300

    stickers
    Задание 8

--- test_sticker.py
+++ test_sticker.py
@@ -3,5 +3,39 @@ import time
 from selenium import webdriver
 from driver_fixture import *
 
+#Сделайте сценарий, проверяющий наличие стикеров у всех товаров в учебном приложении litecart на главной странице.
+# Стикеры -- это полоски в левом верхнем углу изображения товара, на которых написано New или Sale или что-нибудь другое.
+# Сценарий должен проверять, что у каждого товара имеется ровно один стикер.
+
 def test_sticker_on(driver):
-    driver.get("http://localhost/litecart/admin/")
+    driver.get("http://localhost/litecart")
+    time.sleep(3)
+
+    products = driver.find_elements_by_xpath("//li[@class='product column shadow hover-light']")
+    print("There are " + str(len(products)) + " ducks")
+
+    for one in products:
+        stickers = one.find_elements_by_xpath("//div[starts-with(@class,'sticker')]")
+        print ("There are " + str(len(stickers)) + " stickers")
+
+    assert len(products) == len(stickers)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
