c1412326a83ca8f223b0492111ab8e0b181d5c09
mirrkka <aleksandraryzhkova14@gmail.com>
2018-10-03 16:07:46 +0300

    Новый пользователь (регистрация)
    Новый пользователь (регистрация)

--- test_new_costumer.py
+++ test_new_costumer.py
@@ -3,12 +3,32 @@ import time
 from selenium import webdriver
 from driver_fixture import *
 from selenium.webdriver.support.ui import Select
+import random
+import string
 
 
 def login(driver):
     driver.get("http://litecart.stqa.ru/en/")
 
+domains = [ "hotmail.com", "gmail.com", "aol.com", "mail.com" , "mail.kz", "yahoo.com"]
+letters = string.ascii_lowercase[:12]
+
+def get_random_domain(domains):
+    return random.choice(domains)
+
+def get_random_name(letters, length):
+    return ''.join(random.choice(letters) for i in range(length))
+
+def generate_random_emails(nb, length):
+    return [get_random_name(letters, length) + '@' + get_random_domain(domains) for i in range(nb)]
+
+def email_generate():
+    return (generate_random_emails(1, 7))
+
+
+
 def test_new_costumer(driver):
+    email = email_generate()
     login(driver)
     driver.find_element_by_xpath("//a[@href='http://litecart.stqa.ru/en/create_account']").click()
     time.sleep(2)
@@ -31,12 +51,44 @@ def test_new_costumer(driver):
     driver.find_element_by_xpath("//input[@type='search']").send_keys("united states")
     select = Select(driver.find_element_by_name("country_code"))
     select.select_by_visible_text("United States")
-    driver.find_element_by_name("zone_code").click()
-    select_state = Select(driver.find_element_by_name("zone_code"))
-    select_state.select_by_visible_text("California")
+    time.sleep(1)
+    select_state = Select(driver.find_element_by_css_selector("select[name='zone_code']"))
+    select_state.select_by_visible_text("Washington")
     time.sleep(1)
     driver.find_element_by_xpath("//input[@name='phone']").click()
     driver.find_element_by_xpath("//input[@name='phone']").send_keys("+18452245869")
+    time.sleep(1)
+    driver.find_element_by_xpath("//input[@name='email']").click()
+    driver.find_element_by_xpath("//input[@name='email']").send_keys(email[0])
+    time.sleep(1)
+    driver.find_element_by_xpath("//input[@name='password']").click()
+    driver.find_element_by_xpath("//input[@name='password']").send_keys("test")
+
+    driver.find_element_by_xpath("//input[@name='confirmed_password']").click()
+    driver.find_element_by_xpath("//input[@name='confirmed_password']").send_keys("test")
+    time.sleep(1)
+    driver.find_element_by_xpath("//button[@name='create_account']").click()
+    time.sleep(2)
+    driver.find_element_by_xpath("//a[@href='http://litecart.stqa.ru/en/logout']").click()
+    time.sleep(1)
+    driver.find_element_by_xpath("//input[@name='email']").click()
+    driver.find_element_by_xpath("//input[@name='email']").send_keys(email[0])
+
+    driver.find_element_by_xpath("//input[@name='password']").click()
+    driver.find_element_by_xpath("//input[@name='password']").send_keys("test")
+
+    driver.find_element_by_xpath("//button[@name='login']").click()
+
+    driver.find_element_by_xpath("//a[@href='http://litecart.stqa.ru/en/logout']").click()
+
+
+
+
+
+
+
+
+
 
 
 
