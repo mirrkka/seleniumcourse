2b79624ea3c44bda657a8bb8da09c029b6e7b2db
mirrkka <aleksandraryzhkova14@gmail.com>
2018-10-10 15:23:51 +0300

    задание 13
    добавление в корзину и удаление из корзины

--- test_three_items_to_the_cart.py
+++ test_three_items_to_the_cart.py
@@ -7,6 +7,8 @@ from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 from selenium.webdriver import ActionChains
+from selenium.common.exceptions import TimeoutException
+from selenium.webdriver.support.ui import Select
 
 #Сделайте сценарий для добавления товаров в корзину и удаления товаров из корзины.
 #
@@ -18,59 +20,96 @@ from selenium.webdriver import ActionChains
 #5) открыть корзину (в правом верхнем углу кликнуть по ссылке Checkout)
 #6) удалить все товары из корзины один за другим, после каждого удаления подождать, пока внизу обновится таблица
 
-def login(driver):
+
+def three_items(driver):
+    #добавляем три товара
     driver.get("http://litecart.stqa.ru/en/")
+    time.sleep(2)
+    driver.find_element_by_xpath(".//li[@class='product column shadow hover-light']").click()
+    try:
+        present = WebDriverWait(driver, 2).until(
+            EC.presence_of_element_located((By.XPATH, "//select[@name='options[Size]']")))
+        if present:
+            WebDriverWait(driver, 2).until(
+                    EC.presence_of_element_located((By.XPATH, "//select[@name='options[Size]']")))
+            select_state = Select(driver.find_element_by_css_selector("select[name='options[Size]']"))
+            select_state.select_by_visible_text("Small")
+    except TimeoutException:
+        print ("nothing is here")
+
+    time.sleep(1)
+    quantity = driver.find_element_by_xpath("//a[@href='http://litecart.stqa.ru/en/checkout']//span[@class='quantity']").get_attribute('textContent')
+    plusitem = int(quantity) + 1
+    allitems = str(plusitem)
+    print(quantity)
+    driver.find_element_by_xpath("//button[@name='add_cart_product']").click()
+    wait = WebDriverWait(driver, 2)
+    wait.until(EC.text_to_be_present_in_element((By.XPATH,"//a[@href='http://litecart.stqa.ru/en/checkout']//span[@class='quantity']"), allitems))
+    new_quantity = driver.find_element_by_xpath("//a[@href='http://litecart.stqa.ru/en/checkout']//span[@class='quantity']").get_attribute('textContent')
+    assert allitems == new_quantity
+    print(new_quantity)
+    driver.find_element_by_xpath("//a[@href='http://litecart.stqa.ru/en/']").click()
 
 def test_three_items(driver):
-    #добавляем три товара
-    login(driver)
-    time.sleep(5)
-    for i in range(1,4):
-      items = driver.find_elements_by_xpath("//a[@class='listing-wrapper products']//li")
-      #print(str(len(items)))
-    for k in items:
-        k.find_element_by_xpath("./a[@class='link']").click()
-        driver.find_element_by_xpath("//button[@name='add_cart_product']").click()
+    three_items(driver)
+    three_items(driver)
+    three_items(driver)
+    deleting(driver)
+
+
+def deleting(driver):
+    driver.get("http://litecart.stqa.ru/en/checkout")
+    # проверяем таблицу для удаления
+    table = driver.find_elements_by_xpath(".//*[@id='order_confirmation-wrapper']/table/tbody/tr/td[@class='unit-cost']")
+    len_table = len(table)
+    print("Total " + str(len(table)))
+
+    while True:
+            print("something")
+
+            new_len_table = 0
+            try:
+                driver.find_element_by_name("remove_cart_item").click()
+                time.sleep(5)
+                new_len_table = len(driver.find_elements_by_xpath(".//*[@id='order_confirmation-wrapper']/table/tbody/tr/td[@class='unit-cost']"))
+            except:
+                new_len_table = 0
+
+            assert len_table == new_len_table + 1
+            len_table = new_len_table
+
+            if len_table == 0:
+                break
 
 
 
-#    time.sleep(5)
-#    quantity = driver.find_element_by_xpath("//a[@href='http://litecart.stqa.ru/en/checkout']//span[@class='quantity']").get_attribute('textContent')
-#    plusitem = int(quantity) + 1
-#    allitems = str(plusitem)
-#    print(quantity)
-#    driver.find_element_by_xpath("//button[@name='add_cart_product']").click()
-#    wait = WebDriverWait(driver, 5)
-#    wait.until(EC.text_to_be_present_in_element((By.XPATH,"//a[@href='http://litecart.stqa.ru/en/checkout']//span[@class='quantity']"), allitems))
-#    new_quantity = driver.find_element_by_xpath("//a[@href='http://litecart.stqa.ru/en/checkout']//span[@class='quantity']").get_attribute('textContent')
-#
-#    assert allitems == new_quantity
-#    print(new_quantity)
-#    driver.find_element_by_xpath("//a[@href='http://litecart.stqa.ru/en/']").click()
-#
-#
-#def deleting(driver):
-#    # проверяем таблицу для удаления
-#    quantity1 = driver.find_element_by_xpath(".//*[@id='order_confirmation-wrapper']/table/tbody/tr/td").get_attribute('textContent')
-#    minus = int(quantity1) - 1
-#    afterdelete = str(minus)
-#    print(afterdelete)
-#
-#    # убираем товар из корзины
-#
-#    driver.find_element_by_xpath("//input[@type='number']").click()
-#    driver.find_element_by_xpath("//input[@type='number']").clear()
-#    driver.find_element_by_xpath("//input[@type='number']").send_keys(minus)
-#    driver.find_element_by_xpath("//button[@name='update_cart_item']").click()
-#
-#    # смотрим, что там в таблице
-#
-#    wait = WebDriverWait(driver, 5)
-#    wait.until(EC.text_to_be_present_in_element((By.XPATH, ".//*[@id='order_confirmation-wrapper']/table/tbody/tr/td"), afterdelete))
-#    new_quantity1 = driver.find_element_by_xpath("//table[@class='dataTable rounded-corners']//td").get_attribute('textContent')
-#
-#    assert afterdelete == new_quantity1
-#    print(new_quantity1)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
